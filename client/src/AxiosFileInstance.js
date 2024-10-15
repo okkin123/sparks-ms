c18677db805cdb8a7484327ad3d92a0d8d238926
Nikko Vincent <imnikkomaaba@gmail.com>
2024-10-15 12:54:07 +0000

    supplier expense table scrollbar pending

--- client/src/AxiosFileInstance.js
+++ client/src/AxiosFileInstance.js
@@ -5,7 +5,7 @@ const cookies = new Cookies();
 // Create an Axios instance
 const AxiosFileInstance = axios.create({
   //baseURL: "http://localhost:4000/",
-  baseURL: "https://reimagined-invention-4rw965xj75ghq599-4000.app.github.dev/",
+  baseURL: "https://4000-okkin123-sparksms-em0guxdrsgp.ws-us116.gitpod.io/",
   headers: {
     "Content-Type": "multipart/form-data"
   }
--- client/src/Pages/Expenses/Project/Components/ListSupplierExpense.js
+++ client/src/Pages/Expenses/Project/Components/ListSupplierExpense.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState, useRef} from 'react' 
-import {Typography, Chip, Link, Box, Stack, Button} from '@mui/material'
+import {Typography, Chip, Link, Box, Stack, Button, TextField, FormControl, Select, MenuItem, FormHelperText, InputLabel, Divider, Alert} from '@mui/material'
 import AxiosInstance from '../../../../AxiosInstance';
 import {
     MaterialReactTable,
@@ -8,6 +8,15 @@ import { theme } from '../../../../Theme';
 import dayjs from 'dayjs';
 import { NumericFormat } from 'react-number-format';
 import SupplierExpenseColumnFilter from './SupplierExpenseColumnFilter';
+import Dialog from '../../../../Components/Dialog';
+import FileUpload from '../../../../Components/FileUpload';
+import * as Yup from 'yup';
+import { useFormik } from 'formik'
+import AxiosFileInstance from '../../../../AxiosFileInstance';
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+
+import LoadingButton from '@mui/lab/LoadingButton';
 
 const parentHeight = window.innerHeight;
 const parentWidth = window.innerWidth;
@@ -185,11 +194,85 @@ const columns=[
     
 ];
 
+const MakePaymentSchema = Yup.object().shape({
+    mode_of_payment: Yup.string().required('This field is required!'),
+    date: Yup.date().required('Date is required'),
+    reference_number: Yup.string()
+    .matches(/^\d+$/, 'Only whole numbers are allowed!')
+    .test('is-required-if', 'This field is required!', function (value) {
+        const { mode_of_payment } = this.parent;
+        if (mode_of_payment === 'Cheque Deposit' || mode_of_payment === 'Online Transfer') {
+            return value !== undefined && value !== null && value !== '';
+        }
+        return true;
+    }),
+    amount: Yup.string()
+      .matches(/^\d*\.?\d*$/, 'Only numbers and decimal points are allowed!')
+      .required('This field is required!')
+  });
+
 export default function ListSupplierExpense(){
    const [projectExpenses, setProjectExpenses] = useState([]);
    const [filteredData, setFilteredData] = useState([]);
    const [rowSelection, setRowSelection] = useState({});
    const [loading, setLoading] = useState(false)
+   const [fileAlert, setFileAlert] = useState(false)
+   const [fileError, setFileError] = useState('')
+   const [amountAlert, setAmountAlert] = useState('');
+   const [makePayment, setMakePayment] = useState({
+    open: false,
+    ref_invoice_numbers: [],
+    total_amount: 0
+   })
+   const formik_make_payment = useFormik({
+        initialValues: {
+            project_expense_id: 0,
+            mode_of_payment: "",
+            reference_number: "",
+            name: "",
+            date: null,
+            amount: "",
+            file: null
+        },
+        validateOnChange: false,
+        validationSchema: MakePaymentSchema,
+        onSubmit: (values, {validateForm})=>{
+                if(values.file === null){
+                    setFileError('Supporting Document is required!')
+                    setFileAlert(true)
+                }else if(parseFloat(values.amount) !== makePayment.total_amount)
+                {
+                    setAmountAlert('The amount is invalid! The total amount is not equal to the amount entered. Please check and try again!')
+                }
+                else{
+                    setLoading({...loading, make_payment: true})
+                    const formData = new FormData();
+                    formData.append('file', values.file);
+                    formData.append('values', JSON.stringify(values))
+                    
+                    AxiosFileInstance.post("/project_expense/insert_payment", formData)
+                    .then(function(response){
+                    if(response.data.status === 'SUCCESS'){
+                        
+                        alert(response.data.message)
+                        window.location.reload()
+                    }else{
+                        console.log(response.data.message)
+                    }
+                    setLoading({...loading, make_payment: false})
+                    })
+                    .catch(function(error){
+                    console.log(error)
+                    })
+                }
+        
+        }
+    })
+
+    const handleFileUpload = (file) => {
+        formik_make_payment.setFieldValue('file', file);
+        setFileAlert(true)
+    };
     
     useEffect(()=>{
         setLoading(true)
@@ -197,6 +280,7 @@ export default function ListSupplierExpense(){
         .then(function(result){
             if(result.data.status === 'SUCCESS'){
                 const fetchedProjectExpenses = result.data.project_expenses.map((element) => ({
+                    project_expense_id: element.project_expense_id,
                     pe_number: element.pe_number,
                     status: element.STATUS,
                     project_name: element.project_name,
@@ -241,11 +325,21 @@ export default function ListSupplierExpense(){
       };
 
 
-    const total_amount_wo_vat = filteredData.reduce((sum, row) => sum + (row.amount_without_vat || 0), 0);
-    const total_vat_amount = filteredData.reduce((sum, row) => sum + (row.vat_amount || 0), 0);
-    const total_amount_with_vat = filteredData.reduce((sum, row) => sum + (row.amount_with_vat || 0), 0);
-    const total_payments = filteredData.reduce((sum, row) => sum + (row.total_payments || 0), 0);
-    const remaining_balance = filteredData.reduce((sum, row) => sum + (row.remaining_balance || 0), 0);
+    const total_amount_wo_vat = filteredData
+    .filter(row => row.status !== 'VOIDED')
+    .reduce((sum, row) => sum + (row.amount_without_vat || 0), 0);
+    const total_vat_amount = filteredData
+    .filter(row => row.status !== 'VOIDED')
+    .reduce((sum, row) => sum + (row.vat_amount || 0), 0);
+    const total_amount_with_vat = filteredData
+    .filter(row => row.status !== 'VOIDED')
+    .reduce((sum, row) => sum + (row.amount_with_vat || 0), 0);
+    const total_payments = filteredData
+    .filter(row => row.status !== 'VOIDED')
+    .reduce((sum, row) => sum + (row.total_payments || 0), 0);
+    const remaining_balance = filteredData
+    .filter(row => row.status !== 'VOIDED')
+    .reduce((sum, row) => sum + (row.remaining_balance || 0), 0);
 
     const stackRef = useRef(null);
     const [boxWidth, setBoxWidth] = useState(0);
@@ -265,11 +359,26 @@ export default function ListSupplierExpense(){
     };
     }, []);
 
+
+    const handleMakePayment = () => {
+        const selectedRowData = Object.keys(rowSelection).map((rx) => {
+            return filteredData.find((ry) => ry.project_expense_id === parseInt(rx) && ry.status !== 'VOIDED');
+        }).filter(Boolean); // Filter out any undefined values if no match is found
+    
+        const totalAmount = selectedRowData.reduce((sum, row) => sum + row.remaining_balance, 0);
+    
+        setMakePayment({
+            open: true,
+            ref_invoice_numbers: selectedRowData,
+            total_amount: totalAmount
+        });
+    };
+
     return(
         <Stack
         direction="column"
         ref={stackRef}
-        sx={{ flexGrow: 1, width: '100%', maxWidth: '100vw' }}
+        sx={{ flexGrow: 1, width: '100%', maxWidth: '100vw'}}
         >
             <SupplierExpenseColumnFilter 
             columns={
@@ -285,8 +394,8 @@ export default function ListSupplierExpense(){
             remaining_balance={remaining_balance}
             />
              <Box sx={{
-                width: boxWidth,
-                overflowX: 'auto',
+                maxWidth: boxWidth,
+                position: 'relative'
             }}>
             <MaterialReactTable
             enableColumnFilters={false}
@@ -319,6 +428,31 @@ export default function ListSupplierExpense(){
                 variant: 'outlined',
             }}
             paginationDisplayMode='pages'
+              muiTableBodyProps={{
+                sx: {
+                    display: 'block',
+                    maxHeight: '400px', // Adjust the height as needed
+                    overflowY: 'auto',
+                    position: 'relative',
+                },
+              }}
+              muiTableHeadProps={{
+                sx: {
+                    display: 'table',
+                    width: '100%',
+                    tableLayout: 'fixed',
+                    position: 'sticky',
+                    top: 0,
+                    zIndex: 1,
+                },
+              }}
+              muiTableProps={{
+                sx: {
+                  display: 'table',
+                  width: '100%',
+                  tableLayout: 'fixed',
+                },
+              }}
             muiToolbarAlertBannerProps={{
                 sx: {
                 display: 'flex',
@@ -328,7 +462,141 @@ export default function ListSupplierExpense(){
                 },
                 children: (
                 <Box sx={{ display: 'flex', gap: 2 }}>
-                    <Button variant="outlined" size="small" color="secondary">
+                    <Button variant="outlined" size="small" color="info" onClick={handleMakePayment}>
+                        MAKE PAYMENT
+                    </Button>
+                    <Dialog open={makePayment.open} content={
+                        <Stack direction="column" spacing={2}>
+                        <Typography varian="subtitle1">MAKE PAYMENT</Typography>
+                        <Typography variant="body2">Reference Invoice Nos.</Typography>
+                        <div style={{height: 100, overflowY: 'auto'}}>
+                        <Stack direction="row" gap={1} justifyContent="flex-start" alignItems="center" flexWrap="wrap">{
+                            makePayment.ref_invoice_numbers.map((ref_invoice_number)=>(
+                                <Chip size="small" label={
+                                    <>
+                                      {ref_invoice_number.invoice_number} - {ref_invoice_number.project_name} (
+                                      <NumericFormat
+                                        value={ref_invoice_number.remaining_balance}
+                                        displayType={'text'}
+                                        thousandSeparator={true}
+                                        decimalScale={2}
+                                        fixedDecimalScale={true}
+                                      /> )
+                                    </> } />
+                            ))
+                        }</Stack></div>
+                        <Stack direction="row" spacing={2} alignItems="center" size="small">
+                            <Typography variant="body2">Total Amount: </Typography>
+                            <Chip size="small" color="success" label={<><NumericFormat
+                            value={makePayment.total_amount}
+                            displayType={'text'}
+                            thousandSeparator={true}
+                            decimalScale={2}
+                            fixedDecimalScale={true}
+                            /></>} />
+                        </Stack>
+                        <Divider />
+                        {amountAlert && <Alert severity="error">{amountAlert}</Alert> }
+                        <FormControl
+                            fullWidth
+                            size="small"
+                            error={formik_make_payment.touched.mode_of_payment && Boolean(formik_make_payment.errors.mode_of_payment)}
+                            >
+                            <InputLabel>Mode of Payment</InputLabel>
+                            <Select
+                            name="mode_of_payment"
+                            value={formik_make_payment.values.mode_of_payment}
+                            label="Mode of Payment"
+                            onChange={(event)=>formik_make_payment.setFieldValue('mode_of_payment',event.target.value)}
+                            >
+                                <MenuItem value={'Cheque Deposit'}>
+                                    Cheque Deposit
+                                </MenuItem>
+                                <MenuItem value={'Online Transfer'}>
+                                    Online Transfer
+                                </MenuItem>
+                                <MenuItem value={'Cash'}>
+                                    Cash
+                                </MenuItem>
+                            </Select>
+                            <FormHelperText>
+                            {formik_make_payment.touched.mode_of_payment && formik_make_payment.errors.mode_of_payment}
+                            </FormHelperText>
+                        </FormControl> 
+                        
+                       <TextField label="Reference No." size="small" variant="outlined" name="reference_number" fullWidth 
+                        onChange={formik_make_payment.handleChange} value={formik_make_payment.values.reference_number} 
+                        error={
+                            formik_make_payment.touched.reference_number && Boolean(formik_make_payment.errors.reference_number)
+                        }
+                        helperText={
+                            formik_make_payment.touched.reference_number && formik_make_payment.errors.reference_number
+                        } /> 
+                
+                        <Stack direction="row" spacing={2}>
+                            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                <DatePicker 
+                                value={dayjs(formik_make_payment.values.date)}
+                                onChange={(value)=>formik_make_payment.setFieldValue('date', dayjs(new Date(value)).format('YYYY-MM-DD'))}
+                                slotProps={{
+                                    textField: {
+                                    label: 'Date',
+                                    variant: 'outlined',
+                                    name: 'date',
+                                    size: 'small', 
+                                    fullWidth: true,
+                                    error: Boolean(formik_make_payment.errors.date),
+                                    helperText:formik_make_payment.touched.date && formik_make_payment.errors.date
+                                    },
+                                }} />
+                            </LocalizationProvider>
+                            <TextField label="Amount" name="amount" onChange={formik_make_payment.handleChange} size="small" variant="outlined" 
+                            fullWidth value={formik_make_payment.values.amount}
+                            error={
+                                formik_make_payment.touched.amount && Boolean(formik_make_payment.errors.amount)
+                            }
+                            helperText={
+                                formik_make_payment.touched.amount && formik_make_payment.errors.amount
+                            } />
+                        </Stack>
+                    
+                        <Typography variant="subtitle1">Supporting Doc:</Typography>
+                        <FileUpload onFileUpload={handleFileUpload} fileTypes={['application/pdf']} mainError={fileError} alertOpen={fileAlert} />
+                        <Stack direction="row" justifyContent="flex-end" spacing={2}>
+                            <Button size="small" onClick={()=>{
+                                setMakePayment({
+                                    open: false,
+                                    ref_invoice_numbers: [],
+                                    total_amount: 0
+                                })
+                                // formik_make_payment.setValues({
+                                //     project_expense_id: 0,
+                                //     mode_of_payment: "",
+                                //     cheque_no: "",
+                                //     name: "",
+                                //     date: null,
+                                //     amount: "",
+                                //     file: null
+                                // })
+                                setMakePayment({...makePayment, open: false})
+                            }}>Cancel</Button>
+                            <LoadingButton variant='contained' color='secondary' onClick={formik_make_payment.handleSubmit} loading={loading.make_payment}>Save Payment</LoadingButton>
+                        </Stack>  
+                        </Stack>
+
+                    } />
+                    <div
+                        style={{
+                        position: 'absolute',
+                        top: 0,
+                        right: 0,
+                        width: '16px', // Adjust width as needed
+                        height: '100%',
+                        backgroundColor: 'transparent',
+                        pointerEvents: 'none',
+                        }}
+                    />
+                    <Button variant="outlined" size="small" color="success">
                         GENERATE STATEMENT
                     </Button>
                 </Box>
